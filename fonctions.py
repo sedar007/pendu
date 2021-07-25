85a1bba23bd79cf82fad0ed77bb9cb60cb2ca22f
Adjy Sedar Desir <desiradjysedar@gmail.com>
2021-07-25 15:28:43 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -29,12 +29,8 @@ nb_coups = 8
 
 
 def recup_scores():
-    """Cette fonction récupère les scores enregistrés si le fichier existe.
-    Dans tous les cas, on renvoie un dictionnaire, 
-    soit l'objet dépicklé,
-    soit un dictionnaire vide.
-
-    On s'appuie sur nom_fichier_scores défini dans donnees.py"""
+    """ récupère les scores enregistrés si le fichier existe.
+    """
     
     if os.path.exists(nom_fichier_scores): # Le fichier existe
         # On le récupère
@@ -47,9 +43,8 @@ def recup_scores():
     return scores
 
 def enregistrer_scores(scores):
-    """Cette fonction se charge d'enregistrer les scores dans le fichier
-    nom_fichier_scores. Elle reçoit en paramètre le dictionnaire des scores
-    à enregistrer"""
+    """Enregistre les scores dans le fichier
+    nom_fichier_scores. """
 
     fichier_scores = open(nom_fichier_scores, "wb") # On écrase les anciens scores
     mon_pickler = pickle.Pickler(fichier_scores)
@@ -59,12 +54,9 @@ def enregistrer_scores(scores):
 # Fonctions gérant les éléments saisis par l'utilisateur
 
 def recup_nom_utilisateur():
-    """Fonction chargée de récupérer le nom de l'utilisateur.
-    Le nom de l'utilisateur doit être composé de 4 caractères minimum,
+    """Fonction chargée de récupérer le nom de l'utilisateur de 4 caractères minimum,
     chiffres et lettres exclusivement.
-
-    Si ce nom n'est pas valide, on appelle récursivement la fonction
-    pour en obtenir un nouveau"""
+"""
 
     nom_utilisateur = input("Tapez votre nom: ")
     # On met la première lettre en majuscule et les autres en minuscules
